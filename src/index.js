05c6ab53f93612e99a2eda6c6773c06ac90934c6
rishisuyal <rishisuyal@gmail.com>
2023-11-29 13:29:34 +0530

    added webpack and babelrc

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ child_element.innerText = render
 parent_element.appendChild(child_element)*/
 
 // new but hard react way
+import React from "react"
+import ReactDOM  from "react-dom/client"
 const child_element1 = React.createElement('p',{},"hello world")
 const child_element2 = React.createElement('p',{},"hello world")
+const child_element3 = <>hellow</>
 const parent_element = ReactDOM.createRoot(document.getElementById('root'))
-parent_element.render([child_element1,child_element2])
+parent_element.render(child_element3)
