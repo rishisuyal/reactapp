b9a41e4921bfc86b13c4dc3fe6e21347cf480baa
rishisuyal <rishisuyal@gmail.com>
2023-11-29 15:16:28 +0530

    conditional rendering

--- src/index.js
+++ src/index.js
@@ -8,8 +8,13 @@ parent_element.appendChild(child_element)*/
 // new but hard react way
 import React from "react"
 import ReactDOM  from "react-dom/client"
-const child_element1 = React.createElement('p',{},"hello world")
-const child_element2 = React.createElement('p',{},"hello world")
-const child_element3 = <>hellow</>
+function handleClick(msg){
+    alert('pressed '+ msg)
+}
+const condition = false
+const child_element1 = React.createElement('p',{onClick:()=> handleClick('check0')},"hello world0")
+const child_element2 = React.createElement('p',{onClick:()=> handleClick('check1')},"hello world1")
+const child_element3 = <div onClick={()=> handleClick(msg)}>hellow</div>
+const ChildElement4 = ()=><>{condition ? child_element2 : child_element1}</>
 const parent_element = ReactDOM.createRoot(document.getElementById('root'))
-parent_element.render(child_element3)
+parent_element.render(ChildElement4())
